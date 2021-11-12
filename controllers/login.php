a5b1e8a1c7095a2efe3ad1239e98805cdb766dec
Djamin C <cyrilledjamin@yahoo.fr>
2021-11-12 10:47:23 +0100

    nettoyage des commentaire

--- controllers/login.php
+++ controllers/login.php
@@ -7,14 +7,11 @@ if (session_status() === PHP_SESSION_NONE) {
 require_once('models/user.php');
 
 if(isset($_SESSION['user'])){
-    //die("FFF");
     header('Location: index.php?page=dashboard');
 } else if(isset($_POST['email']) && isset($_POST['password'])) {
-    //die("NNN");
     $user = login($_POST['email'], $_POST['password']);
     $_SESSION['user'] = $user;
-    //var_dump(isset($_SESSION['user']));die;
-    // var_dump($user);die;
+
     header('Location: index.php?page=dashboard');
 } else {
     require_once('views/login.php');
--- models/user.php
+++ models/user.php
@@ -3,23 +3,13 @@ function login($email, $password) {
     global  $bdd; 
 
     $_email = isset($email) ? $email : null;
-    //$hash = password_hash($password, PASSWORD_BCRYPT);
-    $_password = isset($password) ? $password : null;
+    $_password = isset($password) ? $password : null;    
 
-    //echo password_hash($password, PASSWORD_BCRYPT); die;
-    //echo $_password; die;
-    //die($password);
-
-    
-
-    $req = $bdd->prepare("SELECT * FROM user WHERE email = :email AND activated=1"); 
+    $req = $bdd->prepare("SELECT * FROM user WHERE email = :email AND activated = 1"); 
     $req->bindParam(':email', $_email, PDO::PARAM_STR); 
     $req->execute();
 
     $user = $req->fetch(); 
-
-    //die($user['password']);
-    //var_dump(password_verify($_password, $user['password'])); die;
 
     if(!empty($user) && password_verify($_password, $user['password'])) {
 
